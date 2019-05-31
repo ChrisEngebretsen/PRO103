4cde5cceef43c655ed3e0da485a7eafbdceeac0c
ChrisEngebretsen <christian.meyer2222@gmail.com>
2019-05-31 10:31:52 +0200

    fikset mainpage

--- mainpage/mainpage.js
+++ mainpage/mainpage.js
@@ -10,3 +10,11 @@ function drop(ev) {
     var data = ev.dataTransfer.getData("text");
     ev.target.appendChild(document.getElementById(data));
 }
+
+function createNewDiv(){
+    var div = document.createElement("div");
+    document.getElementById("to_do_column").appendChild(div);
+    div.setAttribute("id", "newbox" + 1);
+    div.setAttribute("ondragover", "allowDrop(event)");
+    div.setAttribute("ondrop", "drop(event)")
+}
