b431dbc9bd343a2a538022f7851cfd9983c777bb
ChrisEngebretsen <christian.meyer2222@gmail.com>
2019-06-03 18:16:55 +0200

    oppdatert mp

--- mainpage/mainpage.js
+++ mainpage/mainpage.js
@@ -19,7 +19,64 @@ function createNewDiv(){
     taskinput.setAttribute("placeholder", "New taskname..");
     taskinput.setAttribute("class", "taskinputclass");
 
-    //
+
+    // Person selection div
+
+    var personlogodiv = document.createElement("div");
+    document.getElementById("newbox" + count).appendChild(personlogodiv);
+    personlogodiv.setAttribute("class", "personlogodiv");
+    personlogodiv.setAttribute("id", "personlogodiv" + count);
+
+    //Person select
+
+    var personselect = document.createElement("select");
+    document.getElementById("personlogodiv" + count).appendChild(personselect);
+    personselect.setAttribute("class", "personselectclass");
+    personselect.setAttribute("id", "personselect" + count);
+
+
+    // Person select options
+    var personoption0 = document.createElement("option");
+    personoption0.setAttribute("value", "   ");
+    var person0 = document.createTextNode("   ");
+    personoption0.appendChild(person0);
+    document.getElementById("personselect" + count).appendChild(personoption0);
+
+    var personoption1 = document.createElement("option");
+    personoption1.setAttribute("value", "Rahil");
+    var person1 = document.createTextNode("Rahil");
+    personoption1.appendChild(person1);
+    document.getElementById("personselect" + count).appendChild(personoption1);
+
+    var personoption2 = document.createElement("option");
+    personoption2.setAttribute("value", "Christian");
+    var person2 = document.createTextNode("Christian");
+    personoption2.appendChild(person2);
+    document.getElementById("personselect" + count).appendChild(personoption2);
+
+    var personoption3 = document.createElement("option");
+    personoption3.setAttribute("value", "Mohamed");
+    var person3 = document.createTextNode("Mohamed");
+    personoption3.appendChild(person3);
+    document.getElementById("personselect" + count).appendChild(personoption3);
+
+    var personoption4 = document.createElement("option");
+    personoption4.setAttribute("value", "Martin");
+    var person4 = document.createTextNode("Martin");
+    personoption4.appendChild(person4);
+    document.getElementById("personselect" + count).appendChild(personoption4);
+
+    var personoption5 = document.createElement("option");
+    personoption5.setAttribute("value", "Kristine");
+    var person5 = document.createTextNode("Kristine");
+    personoption5.appendChild(person5);
+    document.getElementById("personselect" + count).appendChild(personoption5);
+
+    var personoption6 = document.createElement("option");
+    personoption6.setAttribute("value", "Stine");
+    var person6 = document.createTextNode("Stine");
+    personoption6.appendChild(person6);
+    document.getElementById("personselect" + count).appendChild(personoption6);
 
     //to do, in progress, do today, done options + select bar
 
@@ -115,4 +172,5 @@ function createNewDiv(){
     prioritylow.appendChild(priority3);
     document.getElementById("priorityselect" + count).appendChild(prioritylow);
     document.getElementById("lowpriority" + count).style.color = "#add8e6";
+
 }
