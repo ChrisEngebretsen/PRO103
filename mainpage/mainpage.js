6c7af8ddce1054ef0bde45bbce7b182c05793d22
ChrisEngebretsen <christian.meyer2222@gmail.com>
2019-06-03 16:28:59 +0200

    videreutvikling av mp

--- mainpage/mainpage.js
+++ mainpage/mainpage.js
@@ -5,32 +5,39 @@ count = 0;
 function createNewDiv(){
     count += 1;
 
+    //code to create new row
+
     var div = document.createElement("div");
     document.getElementById("to_do_column").appendChild(div);
     div.setAttribute("id", "newbox" + count);
     div.setAttribute("class", "newboxclass");
 
+    //inputfield in each row
 
     var taskinput = document.createElement("input");
     document.getElementById("newbox" + count).appendChild(taskinput);
-    taskinput.setAttribute("class", "inputclassname");
     taskinput.setAttribute("placeholder", "New taskname..");
     taskinput.setAttribute("class", "taskinputclass");
 
+    //
+
+    //to do, in progress, do today, done options + select bar
 
     var statusdiv = document.createElement("div");
     document.getElementById("newbox" + count).appendChild(statusdiv);
     statusdiv.setAttribute("class", "statusdivclass");
     statusdiv.setAttribute("id", "statusdiv" + count);
 
-    //
-
-    //to do, in progress, do today, done options + select bar
-
     var statusoptions = document.createElement("select");
     document.getElementById("statusdiv" + count).appendChild(statusoptions);
     statusoptions.setAttribute("id", "statusoptionbar" + count);
     statusoptions.setAttribute("class", "statusoptionbarclass");
+
+    var statusoption0 = document.createElement("option");
+    statusoption0.setAttribute("value", "---")
+    var option0 = document.createTextNode("---");
+    statusoption0.appendChild(option0);
+    document.getElementById("statusoptionbar" + count).appendChild(statusoption0);
 
     var statusoption1 = document.createElement("option");
     statusoption1.setAttribute("value", "To-do")
@@ -58,6 +65,54 @@ function createNewDiv(){
 
     //to do, in progress, do today, done options + select bar end
 
+    //date input
 
+    var dateinput = document.createElement("input");
+    document.getElementById("newbox" + count).appendChild(dateinput);
+    dateinput.setAttribute("class", "dateinputclass");
+    dateinput.setAttribute("placeholder", "Set due date..");
+
+    //date input end
+
+    //priority select
+
+    var prioritydiv = document.createElement("div");
+    document.getElementById("newbox" + count).appendChild(prioritydiv);
+    prioritydiv.setAttribute("class", "prioritydivclass");
+    prioritydiv.setAttribute("id", "prioritydiv" + count);
+
+    var priorityselect = document.createElement("select");
+    document.getElementById("prioritydiv" + count).appendChild(priorityselect);
+    priorityselect.setAttribute("id", "priorityselect" + count);
+    priorityselect.setAttribute("class", "priorityselectclass");
+
+    var prioritynull = document.createElement("option");
+    prioritynull.setAttribute("value", "---");
+    var priority0 = document.createTextNode("---");
+    prioritynull.appendChild(priority0);
+    document.getElementById("priorityselect" + count).appendChild(prioritynull);
+
+    var priorityhigh = document.createElement("option");
+    priorityhigh.setAttribute("value", "High");
+    priorityhigh.setAttribute("id", "highpriority" + count);
+    var priority1 = document.createTextNode("High");
+    priorityhigh.appendChild(priority1);
+    document.getElementById("priorityselect" + count).appendChild(priorityhigh);
+    document.getElementById("highpriority" + count).style.color = "#ff0000";
+
+    var prioritymedium = document.createElement("option");
+    prioritymedium.setAttribute("value", "Medium");
+    prioritymedium.setAttribute("id", "mediumpriority" + count);
+    var priority2 = document.createTextNode("Medium");
+    prioritymedium.appendChild(priority2);
+    document.getElementById("priorityselect" + count).appendChild(prioritymedium);
+    document.getElementById("mediumpriority" + count).style.color = "#e5e500";
+
+    var prioritylow = document.createElement("option");
+    prioritylow.setAttribute("value", "Low");
+    prioritylow.setAttribute("id", "lowpriority" + count);
+    var priority3 = document.createTextNode("Low");
+    prioritylow.appendChild(priority3);
+    document.getElementById("priorityselect" + count).appendChild(prioritylow);
+    document.getElementById("lowpriority" + count).style.color = "#add8e6";
 }
-
