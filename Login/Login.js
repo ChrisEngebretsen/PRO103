9897f3f987860afd927ad99dd0b04524695d23de
ChrisEngebretsen <christian.meyer2222@gmail.com>
2019-06-06 10:09:50 +0200

    login ferdig

--- Login/Login.js
+++ Login/Login.js
@@ -2,9 +2,8 @@
 function myFunction(){
     var ID1 = "brukernavn";
     var PW1 = "passord123";
-    var GG = "Login Successful";
+    var GG = "Proceed";
     var BG = "Login Unsuccessful";
-
 var userIdInput = document.getElementById("username").value;
 var userPwInput = document.getElementById("password").value;
 
